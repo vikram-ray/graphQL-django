c2b420a086eb710c1513b2c5a13af7090ef32740
rayvikram <vik8876@gmail.com>
2021-08-08 20:37:26 +0530

    added extensions

--- graphql_django/settings.py
+++ graphql_django/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'graphene_django',                                                                                                                                                                                                                                                                                                                                                                                              
-    'movies'
+    'movies',
+    'django_extensions',
 ]
 
 GRAPHENE = {
