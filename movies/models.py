515a2d993ca264226da6abab752d7a8c93709e0a
rayvikram <vik8876@gmail.com>
2021-08-08 20:12:20 +0530

    added str in models

--- movies/models.py
+++ movies/models.py
@@ -5,14 +5,24 @@ class Actor(models.Model):
     age = models.IntegerField()
     married = models.BooleanField()
     address = models.CharField(max_length=256)
+
+    def __str__(self):
+        return self.name
+    
       
 
 class Director(models.Model):
     name = models.CharField(max_length=256)
     fav_actor = models.ForeignKey(Actor, on_delete=models.PROTECT)
     
+    def __str__(self):
+        return self.name
     
 class Movie(models.Model):
     name = models.CharField(max_length=256)
     director = models.ForeignKey(Director, on_delete=models.PROTECT)
     actor = models.ForeignKey(Actor, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.name
+    
