2276e18c41d07b6086f633da844162482b80ea03
rayvikram <vik8876@gmail.com>
2022-02-19 23:17:16 +0530

    added circle CI

--- movies/tests.py
+++ movies/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from movies.models import Actor
 
-# Create your tests here.
+class ActorTestCase(TestCase):
+    def setUp(self):
+        Actor.objects.create(name="Vikram", age=25, married=False, address="India")
+
+    def test_Actors_can_speak(self):
+        """Actors have name"""
+        vikram = Actor.objects.get(name="Vikram")
+        self.assertEqual(vikram.name, 'Vikram')
