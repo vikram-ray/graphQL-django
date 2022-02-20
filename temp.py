bde3b610744580032042cce275f49bc9b995a420
rayvikram <vik8876@gmail.com>
2022-02-20 17:23:39 +0530

    added env values

--- temp.py
+++
@@ -1,8 +0,0 @@
-from pywebcopy import save_webpage
-
-url = 'http://example.com'
-download_folder = './test-results/'    
-
-kwargs = {'bypass_robots': True, 'project_name': 'example'}
-
-save_webpage(url, download_folder, **kwargs)
